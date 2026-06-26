6d6537c91d6fcb51dc465f97112ea8df00e2af5e
Diego Apugliese <dapugliese@gmail.com>
2026-06-25 21:30:00 -0300

    primera version ejercicio herencia

---
+++ Clases.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Drawing;
+
+namespace miLibreria{
+
+    class Transporte{
+        public int matricula;
+        public string color;
+
+        public int capacidad;
+
+        public void arrancar(){
+
+            Console.WriteLine("Arrancando...");
+        }
+
+        public void detenerse(){
+
+            Console.WriteLine("Deteniendose...");
+        }
+    }
+
+    class Avion : Transporte{
+
+        public string piloto;
+
+
+        public void despegar(){
+
+            Console.WriteLine("Despegando...");
+        }
+
+
+        public void aterrizar(){
+
+            Console.WriteLine("Aterrizando...");
+        }
+
+    }
+
+
+    class Auto : Transporte{
+
+        public bool vtv;
+        public int tarjetaVerdeNro;
+
+        public void cambiarAceite(){
+            Console.WriteLine("Cambiando aceite...");
+        }
+
+    }
+}
---
+++ ProgramaPrincipal.cs
@@ -0,0 +1,15 @@
+using miLibreria;
+
+class principal{
+
+    static void Main(){
+
+        Auto clio = new Auto();
+
+        Avion superJet = new Avion();
+
+
+        
+
+    }
+}
